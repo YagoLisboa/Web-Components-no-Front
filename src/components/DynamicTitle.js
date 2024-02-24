f1f70aa0a688fe6a2c14949d060be3683272f57d
YagoLisboa <yago.lisboa@live.com>
2024-02-24 02:56:19 -0300

    Estilizando um Component

---
+++ src/components/DynamicTitle.js
@@ -0,0 +1,24 @@
+class DynamicTitle extends HTMLElement {
+    constructor() {
+        super();
+
+        const shadow = this.attachShadow({ mode: "open"});
+
+        // base do component
+        const componentRoot = document.createElement("h1");
+        componentRoot.textContent = "Minha notícia";
+
+        // estilo do component
+        const style = document.createElement("style");
+        style.textContent = `
+            h1 {
+                color: red;
+            }
+        `
+        // envio para a shadow
+        shadow.appendChild(componentRoot);
+        shadow.appendChild(style);
+    }
+}
+
+customElements.define("dynamic-title", DynamicTitle);
